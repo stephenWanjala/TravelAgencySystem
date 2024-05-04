a5970e9b6fee6d67e59f3d7415b28e2ee95aba14
wanjala stephen <stephenwanjala145@gmail.com>
2024-05-04 21:19:21 +0300

    add Remote Db

--- TravelAgency_backend/src/main/kotlin/com/github/stephenwanjala/Application.kt
+++ TravelAgency_backend/src/main/kotlin/com/github/stephenwanjala/Application.kt
@@ -2,12 +2,20 @@ package com.github.stephenwanjala
 
 import com.github.stephenwanjala.plugins.*
 import io.ktor.server.application.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    val dbName ="TravelAgency_db"
+    val dbPassword =System.getenv("mongoPassword")
+    val db =
+        KMongo.createClient(connectionString = "mongodb+srv://stephenwanjala145:$dbPassword@database.tr3bttl.mongodb.net/$dbName?retryWrites=true&w=majority")
+            .coroutine
+            .getDatabase(dbName)
     configureSecurity()
     configureSerialization()
     configureMonitoring()
