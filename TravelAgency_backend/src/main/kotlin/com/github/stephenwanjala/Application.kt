6f245be8e0f7d64bc51221c48248d9878ee16f17
wanjala stephen <stephenwanjala145@gmail.com>
2024-05-04 20:04:10 +0300

    ktor backend Scaffold

---
+++ TravelAgency_backend/src/main/kotlin/com/github/stephenwanjala/Application.kt
@@ -0,0 +1,16 @@
+package com.github.stephenwanjala
+
+import com.github.stephenwanjala.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureMonitoring()
+    configureHTTP()
+    configureRouting()
+}
